ce33b6ff1d907c7f0954976fb7ecfe9ec6ec534c
honingwon <honingwon@gmail.com>
2013-08-05 06:39:31 +0000

    csjz帐号存取 php支持代码

---
+++ snippets/php/csjz/ga.php
@@ -0,0 +1,42 @@
+<?php
+//get unloggedin account
+//read the first line from a.txt and save the content as an account.
+//do not delete the line
+//export the account through javascript code
+
+header("Content-type: text/javascript");
+
+$account = '';
+
+function myTrim($str)
+{
+	$str = trim($str);
+	$str=strip_tags($str,"");
+	$str=preg_replace("{\t}","",$str);
+	$str=preg_replace("{\r\n}","",$str);
+	$str=preg_replace("{\r}","",$str);
+	$str=preg_replace("{\n}","",$str);
+	$str=preg_replace("{ }","",$str);
+	return $str;
+}
+
+$file = fopen("a.txt","r");
+
+if(!feof($file))
+{
+
+	$account = myTrim(fgets($file));
+	
+	echo '$("#account").val("' . $account . '")';
+	
+	if($account == '')
+	{
+		echo ';$("#account").hide()';
+		echo ';$("form").hide()';
+		echo ';$("body").append("<h1 style=\"width:500px;height:10px;color:#fff;background:#000\">has no acc left</h1>")';
+	}
+	
+}
+  
+fclose($file);
+?>
